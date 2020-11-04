c34d8b97513a9499226a3266584e6a99ab2c96ba
coderpotter <animesh.nighojkar@gmail.com>
2020-11-04 17:55:19 +0000

    #7 implemented sessions; unique code; final_amt calculation

--- apt.py
+++ apt.py
@@ -1,4 +1,5 @@
 import random
+import string
 from time import time
 from math import exp
 
@@ -7,8 +8,9 @@ from simpletransformers.classification import ClassificationModel
 from pandas import DataFrame
 from numpy import argmax
 
-from flask import Flask, request, jsonify, session, render_template
+from flask import Flask, request, jsonify, session, render_template, redirect
 from flask_cors import CORS, cross_origin
+from flask_session import Session
 
 bleurt_scorer = BleurtScorer('bleurt/bleurt/bleurt-base-128/')
 mi_scorer = ClassificationModel('roberta', 'roberta_nli/', use_cuda=False, args = {'reprocess_input_data':True})
@@ -25,46 +27,64 @@ with open('mrpc/msr_paraphrase_train.txt', 'r') as f:
         mrpc.append(l.split('\t'))
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'redis'
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config["DEBUG"] = True
+app.config['SECRET_KEY'] = 'Manvi'
+Session(app)
 
-sentence = ''
+letters_digits = string.ascii_letters + string.digits
 
-@app.route('/form.html', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+@cross_origin()
+def init():
+    session['token'] = ''.join((random.choice(letters_digits) for i in range(10)))
+    session['final_amt'] = 0.0
+    session['sentence'] = None
+    print(session['token'])
+    print(session['final_amt'])
+    return start()
+
+@app.route('/start', methods=['GET', 'POST'])
 @cross_origin()
 def start():
-    global sentence
     print('in start')
-    sentence = str(random.choice(mrpc)[3])
-    print(sentence)
-    return sentence
+    session['sentence'] = str(random.choice(mrpc)[3])
+    print(session['sentence'])
+    print(session['token'])
+    print(session['final_amt'])
+    return render_template("form.html", data=session)
 
 @app.route('/check', methods=['POST'])
 @cross_origin()
 def check_candidate():
     candidate = request.form.get('candidate')
+    print(session['token'])
     print("Candidate:", str(candidate))
-    bleurtscore = (bleurt_scorer.score([sentence], [candidate])[0] + bleurt_scorer.score([candidate], [sentence])[0]) / 2
+    bleurtscore = (bleurt_scorer.score([session['sentence']], [candidate])[0] + bleurt_scorer.score([candidate], [session['sentence']])[0]) / 2
     print("BLEURT:", str(bleurtscore))
-    miscore = get_mi_score([sentence], [candidate])
+    miscore = get_mi_score([session['sentence']], [candidate])
     print("MI:", str(miscore))
     dollars = min(0.5, max(0, miscore - (1 / (1 + exp(-bleurtscore)))))
     print("Dollars:", str(dollars))
     with open('sentences/checks', 'a+') as f:
-        f.write('\t'.join([str(time()), sentence, candidate, str(bleurtscore), str(miscore), str(dollars)]) + '\n')
+        f.write('\t'.join([str(time()), session['sentence'], candidate, str(bleurtscore), str(miscore), str(dollars)]) + '\n')
     return {'candidate':candidate, 'bleurtscore':bleurtscore, 'miscore':miscore, 'dollars':dollars}
 
 @app.route('/submit', methods=['POST'])
 @cross_origin()
 def submit_candidate():
     candidate = request.form.get('candidate')
+    print(session['token'])
     print("Candidate:", str(candidate))
-    bleurtscore = (bleurt_scorer.score([sentence], [candidate])[0] + bleurt_scorer.score([candidate], [sentence])[0]) / 2
-    miscore = get_mi_score([sentence], [candidate])
+    print("Sentence:", str(session['sentence']))
+    bleurtscore = (bleurt_scorer.score([session['sentence']], [candidate])[0] + bleurt_scorer.score([candidate], [session['sentence']])[0]) / 2
+    miscore = get_mi_score([session['sentence']], [candidate])
     dollars = min(0.5, max(0, miscore - (1 / (1 + exp(-bleurtscore)))))
     with open('sentences/submits', 'a+') as f:
-        f.write('\t'.join([str(time()), sentence, candidate, str(bleurtscore), str(miscore), str(dollars)]) + '\n')
-    return {'candidate':candidate, 'bleurtscore':bleurtscore, 'miscore':miscore, 'dollars':dollars}
-
+        f.write('\t'.join([session['token'], str(time()), session['sentence'], candidate, str(bleurtscore), str(miscore), str(dollars)]) + '\n')
+    session['final_amt'] += dollars
+    return start()
+    
 app.run(host='0.0.0.0')
