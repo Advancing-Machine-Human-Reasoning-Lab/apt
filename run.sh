abd9c8229934a9b9f6fa5002d6535652f576f636
coderpotter <animesh.nighojkar@gmail.com>
2020-12-15 22:00:39 -0500

    modified run.sh to run nap_generation

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # python  train-models-on-ppdb.py  twitter-ppdb  roberta  roberta-base  models/twitter-ppdb/roberta-base/
-python  train-models-on-ppdb.py  twitter-ppdb  roberta  roberta-base  models/twitter-ppdb-app/roberta-base/  True
-python  test-models-on-app.py  roberta  models/twitter-ppdb/roberta-base/  models/predictions/twitter-ppdb/roberta-base/
-python  test-models-on-app.py  roberta  models/twitter-ppdb-app/roberta-base/  models/predictions/twitter-ppdb-app/roberta-base/
+# python  train-models-on-ppdb.py  twitter-ppdb  roberta  roberta-base  models/twitter-ppdb-app/roberta-base/  True
+
+# python  test-models-on-app.py  roberta  models/twitter-ppdb/roberta-base/  models/predictions/twitter-ppdb/roberta-base/
+# python  test-models-on-app.py  roberta  models/twitter-ppdb-app/roberta-base/  models/predictions/twitter-ppdb-app/roberta-base/
+
+python  nap_generation.py  0.5  120  0.95  20  0.5
