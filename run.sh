b5701ef78a5502a2563e69800f0d4a092b42ddf2
coderpotter <animesh.nighojkar@gmail.com>
2020-12-19 21:33:24 -0500

    general commit

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@
 
 # python  test-models-on-app.py  roberta  models/twitter-ppdb/roberta-base/  models/predictions/twitter-ppdb/roberta-base/
 # python  test-models-on-app.py  roberta  models/twitter-ppdb-app/roberta-base/  models/predictions/twitter-ppdb-app/roberta-base/
-python  test-models-on-app.py  roberta  models/twitter-ppdb-nap-msrp/roberta-base/  models/predictions/twitter-ppdb-nap-msrp/roberta-base/
+# python  test-models-on-app.py  roberta  models/twitter-ppdb-nap-msrp/roberta-base/  models/predictions/twitter-ppdb-nap-msrp/roberta-base/
 
-# for i in 0 1 2
-# do
-#     python  nap_generation.py  0  ppnmt1  3  $i &
-# done
+for i in 0 1 2
+do
+    python  nap_generation.py  0  ppnmt1  3  $i &
+done
